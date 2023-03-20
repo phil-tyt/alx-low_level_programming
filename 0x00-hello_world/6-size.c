2a6d5033010d1edfd34aa9b74617fa3f64e535b9
Gyambrah Asamoah Philemon <philemon.musicman@gmail.com>
2023-03-20 00:04:50 -0700

    Hello world, here i come

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("size of a char: %d byte(s)\n", size of (char));
@@ -11,5 +11,5 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", size of (long int));
 	printf("size of a long long int: %d byte(s)\n", size of (long long int));
 	printf("size of a float: %d byte(s)\n", size of (float));
-	return (0);
-
+	return (0)
+}
